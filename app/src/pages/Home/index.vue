3729d9ecfdb061458308720dc028d8ffe0ee837f
Lemonadeccc <zwjhb12@163.com>
2022-08-30 14:47:59 +0800

    第一次提交

---
+++ app/src/pages/Home/index.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        我是首页
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'AppIndex',
+
+    data() {
+        return {
+            
+        };
+    },
+
+    mounted() {
+        
+    },
+
+    methods: {
+        
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ app/src/pages/Search/index.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        我是搜索
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'AppIndex',
+
+    data() {
+        return {
+            
+        };
+    },
+
+    mounted() {
+        
+    },
+
+    methods: {
+        
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
