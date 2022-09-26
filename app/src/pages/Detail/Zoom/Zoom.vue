b73ae91c989805a9f2675d46d7ad9125ea59d045
Lemonadeccc <zwjhb12@163.com>
2022-09-26 21:31:59 +0800

    -68p

--- app/src/pages/Detail/Zoom/Zoom.vue
+++ app/src/pages/Detail/Zoom/Zoom.vue
@@ -1,11 +1,12 @@
 <template>
   <div class="spec-preview">
     <img :src="imgObj.imgUrl" />
-    <div class="event"></div>
+    <div class="event" @mousemove="handler"></div>
     <div class="big">
-      <img :src="imgObj.imgUrl" />
+      <img :src="imgObj.imgUrl" ref="big"/>
     </div>
-    <div class="mask"></div>
+    <!-- 遮罩板 -->
+    <div class="mask" ref="mask"></div>
   </div>
 </template>
 
@@ -15,17 +16,53 @@
 
     props:['skuImageList'],
 
+    data(){
+      return {
+        currentIndex:0
+      }
+    },
+
+    methods:{
+      handler(event){
+        let mask = this.$refs.mask;
+        let big = this.$refs.big;
+        let left = event.offsetX - mask.offsetWidth / 2;
+        let top = event.offsetY - mask.offsetHeight / 2;
+        //约束范围
+        if(left <= 0){
+          left = 0;
+        }
+        if(left >= mask.offsetWidth){
+          left = mask.offsetWidth;
+        }
+        if(top = 0){
+          top = 0;
+        }
+        if(top >= mask.offsetHeight){
+          top = mask.offsetHeight;
+        }
+        //修改元素的Left|top属性值
+        mask.style.left = left + 'px';
+        mask.style.top = top + 'px';
+        big.style.left = - 2 * left + 'px';
+        big.style.top = - 2 * top + 'px';
+        // 大图的移动方向和遮罩层相反，
+        // 因为大图放大了2倍，所以移动的距离也放大2倍。
+      }
+    },
+
     mounted(){
       //全局事件总线，获取兄弟组件传递过来的索引值
       this.$bus.$on('getIndex',() => {
-
+        //修改当前响应式数据
+        this.currentIndex = index;
       })
 
     },
 
     computed:{
       imgObj(){
-        return this.skuImageList[0] || {};
+        return this.skuImageList[this.currentIndex] || {};
       }
     },
     
